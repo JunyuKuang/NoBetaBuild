75990639b7306ee5ec96e2e2f2c491d130968b41
Jonny Kuang <lightscreen.app@gmail.com>
2019-07-18 19:11:32 +0800

    Initial Commit

---
+++ NoBetaBuild/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoBetaBuild
+//
+//  Created by Jonny Kuang on 7/18/19.
+//  Copyright © 2019 Jonny Kuang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
