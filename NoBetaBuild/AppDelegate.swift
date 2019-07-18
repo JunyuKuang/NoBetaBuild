48659693a08946444c3d0ac6c1080321c891c420
Jonny Kuang <lightscreen.app@gmail.com>
2019-07-18 20:26:37 +0800

    1.0

--- NoBetaBuild/AppDelegate.swift
+++ NoBetaBuild/AppDelegate.swift
@@ -10,17 +10,21 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    private var window: NSWindow?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        window = NSApp.windows.first
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        window?.makeKeyAndOrderFront(self)
+        return false
+    }
 }
 
--- NoBetaBuild/ViewController.swift
+++ NoBetaBuild/ViewController.swift
@@ -10,18 +10,57 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
+    let buildVersionKey = "buildVersion"
+    
+    /// macOS Mojave 10.14.5
+    let defaultBuildVersion = "18F203"
+    
+    @IBOutlet var textField: NSTextField! {
         didSet {
-        // Update the view, if already loaded.
+            let version = validate(UserDefaults.standard.string(forKey: buildVersionKey))
+            UserDefaults.standard.setValue(version, forKey: buildVersionKey)
         }
     }
-
-
+    
+    @IBAction func openDocument(_ sender: Any) {
+        UserDefaults.standard.set(validate(textField.stringValue), forKey: buildVersionKey)
+        
+        let panel = NSOpenPanel()
+        panel.allowsMultipleSelection = true
+        panel.allowedFileTypes = ["xcarchive"]
+        
+        panel.beginSheetModal(for: view.window!) { result in
+            guard result == .OK else {
+                return
+            }
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.handle(panel.urls)
+            }
+        }
+    }
+    
+    func handle(_ fileURLs: [URL]) {
+        let buildVersion = validate(UserDefaults.standard.string(forKey: buildVersionKey))
+        
+        for url in fileURLs {
+            guard let subpaths = FileManager.default.subpaths(atPath: url.path) else { continue }
+            let infoPlistPaths = subpaths.filter { $0.hasSuffix("/Info.plist") }
+            
+            for path in infoPlistPaths {
+                let plistURL = url.appendingPathComponent(path, isDirectory: false)
+                
+                if let dictionary = NSMutableDictionary(contentsOf: plistURL), dictionary["BuildMachineOSBuild"] != nil {
+                    dictionary["BuildMachineOSBuild"] = buildVersion
+                    let success = dictionary.write(to: plistURL, atomically: true)
+                    print(success)
+                }
+            }
+        }
+    }
+    
+    func validate(_ buildVersion: String?) -> String {
+        let string = (buildVersion ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+        return string.isEmpty ? defaultBuildVersion : string
+    }
 }
 
